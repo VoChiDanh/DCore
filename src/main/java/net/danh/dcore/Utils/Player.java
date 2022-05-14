30124b52148cfdf302e1638be1c5b1064e4e570f
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-05-14 12:31:10 +0700

    Refactor Player#sendPlayerMessage and Player#sendPlayerMessageType

--- src/main/java/net/danh/dcore/Utils/Player.java
+++ src/main/java/net/danh/dcore/Utils/Player.java
@@ -13,21 +13,17 @@ public class Player {
         }
     }
 
-    public static void sendPlayerMessageList(org.bukkit.entity.Player p, List<String> msg) {
+    public static void sendPlayerMessage(org.bukkit.entity.Player p, List<String> msg) {
         for (String string : msg) {
             p.sendMessage(Chat.colorize(string));
         }
     }
 
-    public static void sendPlayerActionBar(org.bukkit.entity.Player p, String... msg) {
-        for (String string : msg) {
-            p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TranslatableComponent(Chat.colorize(string)));
-        }
-    }
-
-    public static void sendPlayerMessageType(org.bukkit.entity.Player p, String type, String... msg) {
-        for (String string : msg) {
-            p.spigot().sendMessage(ChatMessageType.valueOf(type), new TranslatableComponent(Chat.colorize(string)));
+    public static void sendPlayerMessageType(org.bukkit.entity.Player p, String type, String msg) {
+        if (type != null) {
+            p.spigot().sendMessage(ChatMessageType.valueOf(type), new TranslatableComponent(Chat.colorize(msg)));
+        } else {
+            p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TranslatableComponent(Chat.colorize(msg)));
         }
     }
 }
