1f4fd65e20a679792ab6f3b41c062f30f49e4dba
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-05-14 12:22:16 +0700

    add sendPlayerMessageList(p, List<String> msg);

--- src/main/java/net/danh/dcore/Utils/Player.java
+++ src/main/java/net/danh/dcore/Utils/Player.java
@@ -3,9 +3,17 @@ package net.danh.dcore.Utils;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TranslatableComponent;
 
+import java.util.List;
+
 public class Player {
 
     public static void sendPlayerMessage(org.bukkit.entity.Player p, String... msg) {
+        for (String string : msg) {
+            p.sendMessage(Chat.colorize(string));
+        }
+    }
+
+    public static void sendPlayerMessageList(org.bukkit.entity.Player p, List<String> msg) {
         for (String string : msg) {
             p.sendMessage(Chat.colorize(string));
         }
