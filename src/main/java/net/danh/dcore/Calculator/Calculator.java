682db76d7883d524ec0cddb1e1cb10021de8b70d
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-05-15 18:11:24 +0700

    add Calculator#calculator(s, d);

---
+++ src/main/java/net/danh/dcore/Calculator/Calculator.java
@@ -0,0 +1,217 @@
+package net.danh.dcore.Calculator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @version 1.1
+ */
+public class Calculator {
+
+    private static double parsefirst(String s) {
+        s = s.replaceAll(" ", "");
+        if (s.startsWith("@")) {
+            int sqrt = 0;
+            int open = 0;
+            int close = 0;
+            double result = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '@') {
+                    sqrt = i;
+                    break;
+                }
+            }
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '{') {
+                    open = i;
+                    break;
+                }
+            }
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s.charAt(i) == '}') {
+                    close = i;
+                    break;
+                }
+            }
+            if (s.charAt(sqrt + 1) == '{') {
+                double number = Double.parseDouble(calculator(s.substring(open + 1, close), -1));
+                result = Math.sqrt(number);
+            }
+            return result;
+        } else if (s.contains("^")) {
+            List<Integer> pow = new ArrayList<>();
+            double result = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '^') {
+                    pow.add(i);
+                }
+            }
+            for (int i = 0; i < pow.size(); i++) {
+                if (i == 0) {
+                    double a = Double.parseDouble(calculator(s.substring(0, pow.get(i)), -1));
+                    double b;
+                    if (i == pow.size() - 1) b = Double.parseDouble(calculator(s.substring(pow.get(0) + 1), -1));
+                    else b = Double.parseDouble(calculator(s.substring(pow.get(0) + 1, pow.get(1)), -1));
+                    result += Math.pow(a, b);
+                } else if (i == pow.size() - 1) {
+                    double a = Double.parseDouble(calculator(s.substring(pow.get(i) + 1), -1));
+                    result = Math.pow(result, a);
+                } else {
+                    double a = Double.parseDouble(calculator(s.substring(pow.get(i) + 1, pow.get(i + 1)), -1));
+                    result = Math.pow(result, a);
+                }
+            }
+            return result;
+        } else {
+            return Double.parseDouble(s);
+        }
+    }
+
+    private static double parsesecond(String s) {
+        s = s.replaceAll(" ", "");
+        if (s.contains("*") || s.contains("/") || s.contains("%")) {
+            List<Integer> terms = new ArrayList<>();
+            double result = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '*' || s.charAt(i) == '/' || s.charAt(i) == '%') {
+                    terms.add(i);
+                }
+            }
+            for (int i = 0; i < terms.size(); i++) {
+                if (i == 0) {
+                    double a = Double.parseDouble(calculator(s.substring(0, terms.get(i)), -1));
+                    double b;
+                    if (i == terms.size() - 1) b = Double.parseDouble(calculator(s.substring(terms.get(0) + 1), -1));
+                    else b = Double.parseDouble(calculator(s.substring(terms.get(0) + 1, terms.get(1)), -1));
+                    if (s.charAt(terms.get(i)) == '*') result = a * b;
+                    else if (s.charAt(terms.get(i)) == '/') result = a / b;
+                    else if (s.charAt(terms.get(i)) == '%') result = a % b;
+                } else if (i == terms.size() - 1) {
+                    double b = Double.parseDouble(calculator(s.substring(terms.get(i) + 1), -1));
+                    if (s.charAt(terms.get(i)) == '*') result *= b;
+                    else if (s.charAt(terms.get(i)) == '/') result /= b;
+                    else if (s.charAt(terms.get(i)) == '%') result %= b;
+                } else {
+                    double b = Double.parseDouble(calculator(s.substring(terms.get(i) + 1, terms.get(i + 1)), -1));
+                    if (s.charAt(terms.get(i)) == '*') result *= b;
+                    else if (s.charAt(terms.get(i)) == '/') result /= b;
+                    else if (s.charAt(terms.get(i)) == '%') result %= b;
+                }
+            }
+            return result;
+        } else {
+            return Double.parseDouble(s);
+        }
+    }
+
+    private static double parsethird(String s) {
+        s = s.replaceAll(" ", "");
+        List<Integer> operators = new ArrayList<>();
+        double result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+                operators.add(i);
+            }
+        }
+        for (int i = 0; i < operators.size(); i++) {
+            if (i == 0) {
+                double a = Double.parseDouble(calculator(s.substring(0, operators.get(i)), -1));
+                double b;
+                if (i == operators.size() - 1)
+                    b = Double.parseDouble(calculator(s.substring(operators.get(0) + 1), -1));
+                else b = Double.parseDouble(calculator(s.substring(operators.get(0) + 1, operators.get(1)), -1));
+                if (s.charAt(operators.get(i)) == '+') result = a + b;
+                else if (s.charAt(operators.get(i)) == '-') result = a - b;
+            } else if (i == operators.size() - 1) {
+                double b = Double.parseDouble(calculator(s.substring(operators.get(i) + 1), -1));
+                if (s.charAt(operators.get(i)) == '+') result += b;
+                else if (s.charAt(operators.get(i)) == '-') result -= b;
+            } else {
+                double b = Double.parseDouble(calculator(s.substring(operators.get(i) + 1, operators.get(i + 1)), -1));
+                if (s.charAt(operators.get(i)) == '+') result += b;
+                else if (s.charAt(operators.get(i)) == '-') result -= b;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @param Expression The calculator, number + number | number  - number | number *  number | number / number | number%number | number^number | @{number}
+     * @param Demical    the number after .
+     * @return the answer of calculator
+     */
+    public static String calculator(String Expression, int Demical) {
+        boolean b = Expression.contains("*") || Expression.contains("/") || Expression.contains("%");
+        if (Demical <= -1) {
+            if (!Expression.contains("(") && !Expression.contains(")")) {
+                if (Expression.contains("^") || Expression.contains("@")) {
+                    return String.valueOf(parsefirst(Expression));
+                } else if (b) {
+                    return String.valueOf(parsesecond(Expression));
+                } else if (Expression.contains("+") || Expression.contains("-")) {
+                    return String.valueOf(parsethird(Expression));
+                } else {
+                    return Expression;
+                }
+            } else {
+                Expression = Expression.replaceAll(" ", "");
+                List<Integer> open = new ArrayList<>();
+                List<Integer> close = new ArrayList<>();
+                List<String> es = new ArrayList<>();
+                for (int i = 0; i < Expression.length(); i++) {
+                    if (Expression.charAt(i) == '(') open.add(i);
+                }
+                for (int i = Expression.length() - 1; i >= 0; i--) {
+                    if (Expression.charAt(i) == ')') close.add(i);
+                }
+                if (open.size() != close.size()) return Expression;
+                else {
+                    for (int i = 0; i < open.size(); i++) {
+                        if (!Expression.substring(open.get(i) + 1, close.get(i)).contains("(") && !Expression.substring(open.get(i) + 1, close.get(i)).contains(")"))
+                            es.add(Expression.substring(open.get(i) + 1, close.get(i)));
+                        else es.add(calculator(Expression.substring(open.get(i) + 1, close.get(i)), Demical));
+                    }
+                }
+                for (String e : es) {
+                    Expression = Expression.replace("(" + e + ")", calculator(e, Demical));
+                }
+                return calculator(Expression, Demical);
+            }
+        } else {
+            if (!Expression.contains("(") && !Expression.contains(")")) {
+                if (Expression.contains("^") || Expression.contains("@")) {
+                    return String.format("%." + Demical + "f", parsefirst(Expression));
+                } else if (b) {
+                    return String.format("%." + Demical + "f", parsesecond(Expression));
+                } else if (Expression.contains("+") || Expression.contains("-")) {
+                    return String.format("%." + Demical + "f", parsethird(Expression));
+                } else {
+                    return Expression;
+                }
+            } else {
+                Expression = Expression.replaceAll(" ", "");
+                List<Integer> open = new ArrayList<>();
+                List<Integer> close = new ArrayList<>();
+                List<String> es = new ArrayList<>();
+                for (int i = 0; i < Expression.length(); i++) {
+                    if (Expression.charAt(i) == '(') open.add(i);
+                }
+                for (int i = Expression.length() - 1; i >= 0; i--) {
+                    if (Expression.charAt(i) == ')') close.add(i);
+                }
+                if (open.size() != close.size()) return Expression;
+                else {
+                    for (int i = 0; i < open.size(); i++) {
+                        if (!Expression.substring(open.get(i) + 1, close.get(i)).contains("(") && !Expression.substring(open.get(i) + 1, close.get(i)).contains(")"))
+                            es.add(Expression.substring(open.get(i) + 1, close.get(i)));
+                        else es.add(calculator(Expression.substring(open.get(i) + 1, close.get(i)), Demical));
+                    }
+                }
+                for (String e : es) {
+                    Expression = Expression.replace("(" + e + ")", calculator(e, Demical));
+                }
+                return calculator(Expression, Demical);
+            }
+        }
+    }
+}
