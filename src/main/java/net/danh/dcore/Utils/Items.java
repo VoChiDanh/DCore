14679acba668df22bb3d162f002cfaba053d50d2
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-05-16 18:59:21 +0700

    add Items#makeItem(m, d, a, g, hf, u, n, l);

--- src/main/java/net/danh/dcore/Utils/Items.java
+++ src/main/java/net/danh/dcore/Utils/Items.java
@@ -38,8 +38,9 @@ public class Items {
      * @return ItemStack
      */
     public static ItemStack makeItem(Material material, Short data, Integer amount, Boolean glow, Boolean HideFlag, Boolean Unbreakable, String name, List<String> lore) {
+        ItemStack itemStack;
         if (data == null) {
-            ItemStack itemStack = new ItemStack(material, amount);
+            itemStack = new ItemStack(material, amount);
             ItemMeta itemMeta = itemStack.getItemMeta();
             if (name != null) {
                 itemMeta.setDisplayName(Chat.colorize(name));
@@ -62,9 +63,8 @@ public class Items {
                 itemMeta.setUnbreakable(true);
             }
             itemStack.setItemMeta(itemMeta);
-            return itemStack;
         } else {
-            ItemStack itemStack = new ItemStack(material, amount, data);
+            itemStack = new ItemStack(material, amount, data);
             ItemMeta itemMeta = itemStack.getItemMeta();
             if (name != null) {
                 itemMeta.setDisplayName(Chat.colorize(name));
@@ -87,8 +87,81 @@ public class Items {
                 itemMeta.setUnbreakable(true);
             }
             itemStack.setItemMeta(itemMeta);
-            return itemStack;
         }
+        return itemStack;
+    }
+
+    /**
+     * @param material    Material
+     * @param data        Data (For legacy version 1.13 below), null if you use 1.13+
+     * @param amount      int
+     * @param glow        true/false
+     * @param HideFlag    true/false
+     * @param Unbreakable true/false
+     * @param name        Item name
+     * @param lore        Item lore
+     * @return ItemStack
+     */
+    public static ItemStack makeItem(Material material, Short data, Integer amount, Boolean glow, Boolean HideFlag, Boolean Unbreakable, String name, String... lore) {
+        ItemStack itemStack;
+        if (data == null) {
+            itemStack = new ItemStack(material, amount);
+            ItemMeta itemMeta = itemStack.getItemMeta();
+            if (name != null) {
+                itemMeta.setDisplayName(Chat.colorize(name));
+            }
+            if (lore != null) {
+                List<String> l = new ArrayList<>();
+                for (String lores : lore) {
+                    l.add(Chat.colorize(lores));
+                }
+                itemMeta.setLore(Lore(l));
+            }
+            if (glow) {
+                itemMeta.addEnchant(Enchantment.DURABILITY, 1, true);
+                itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            }
+            if (HideFlag) {
+                itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+                itemMeta.addItemFlags(ItemFlag.HIDE_DESTROYS);
+                itemMeta.addItemFlags(ItemFlag.HIDE_PLACED_ON);
+                itemMeta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
+                itemMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+            }
+            if (Unbreakable) {
+                itemMeta.setUnbreakable(true);
+            }
+            itemStack.setItemMeta(itemMeta);
+        } else {
+            itemStack = new ItemStack(material, amount, data);
+            ItemMeta itemMeta = itemStack.getItemMeta();
+            if (name != null) {
+                itemMeta.setDisplayName(Chat.colorize(name));
+            }
+            if (lore != null) {
+                List<String> l = new ArrayList<>();
+                for (String lores : lore) {
+                    l.add(Chat.colorize(lores));
+                }
+                itemMeta.setLore(l);
+            }
+            if (glow) {
+                itemMeta.addEnchant(Enchantment.DURABILITY, 1, true);
+                itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            }
+            if (HideFlag) {
+                itemMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+                itemMeta.addItemFlags(ItemFlag.HIDE_DESTROYS);
+                itemMeta.addItemFlags(ItemFlag.HIDE_PLACED_ON);
+                itemMeta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
+                itemMeta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+            }
+            if (Unbreakable) {
+                itemMeta.setUnbreakable(true);
+            }
+            itemStack.setItemMeta(itemMeta);
+        }
+        return itemStack;
     }
 
 }
