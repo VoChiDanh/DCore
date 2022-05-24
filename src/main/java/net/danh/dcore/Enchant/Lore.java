e9058ecfd004caef5a2cbeab4fc262738328fa9a
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-05-24 11:05:33 +0700

    add removeLore() to fix lore doesn't remove when add new Enchant

--- src/main/java/net/danh/dcore/Enchant/Lore.java
+++ src/main/java/net/danh/dcore/Enchant/Lore.java
@@ -15,6 +15,38 @@ import static net.danh.dcore.Utils.Player.sendPlayerMessage;
 
 public class Lore {
 
+    /**
+     * @param itemStack Item
+     * @param lore      Lore
+     */
+    private static void removeLore(ItemStack itemStack, String lore) {
+        ItemMeta meta = itemStack.getItemMeta();
+        if (meta == null) {
+            return;
+        }
+        if (meta.getLore() == null) {
+            return;
+        }
+        int line = 0;
+        for (int i = 0; i < meta.getLore().size(); i++) {
+            if (meta.getLore().get(i).startsWith(Chat.colorize(lore))) {
+                line = i;
+                break;
+            }
+        }
+
+        meta.getLore().remove(line);
+        itemStack.setItemMeta(meta);
+    }
+
+    /**
+     * @param core      Plugin
+     * @param key       key (upper_case_plz)
+     * @param p         Player
+     * @param itemStack Item
+     * @param lore      lore
+     * @param level     level
+     */
     public static void addEnchant(JavaPlugin core, String key, Player p, ItemStack itemStack, String lore, Integer level) {
         ItemMeta meta = itemStack.getItemMeta();
         if (meta == null) {
@@ -25,20 +57,24 @@ public class Lore {
             sendPlayerMessage(p, "&cKhông thề phù phép vật phẩm này");
             return;
         }
+        removeLore(itemStack, lore);
         List<String> itemlores = new ArrayList<>();
-        if (meta.getLore().contains(Chat.colorize(lore))) {
-            meta.getLore().remove(Chat.colorize(lore + " " + (level - 1)));
-        }
         if (meta.hasLore()) {
             itemlores = meta.getLore();
         }
-        itemlores.add(lore + " " + level);
+        itemlores.add(Chat.colorize(lore + " " + level));
         meta.setLore(itemlores);
         meta.getPersistentDataContainer().set(new NamespacedKey(core, key), PersistentDataType.INTEGER, level);
         itemStack.setItemMeta(meta);
         sendPlayerMessage(p, "&aPhù phép thành công enchant " + lore + " &cLv." + level);
     }
 
+    /**
+     * @param core      Plugin
+     * @param key       key (upper_case_plz)
+     * @param itemStack item
+     * @return true/false
+     */
     public static boolean hasEnchant(JavaPlugin core, String key, ItemStack itemStack) {
         ItemMeta meta = itemStack.getItemMeta();
         if (meta == null) {
@@ -50,6 +86,12 @@ public class Lore {
         return meta.getPersistentDataContainer().has(new NamespacedKey(core, key), PersistentDataType.INTEGER);
     }
 
+    /**
+     * @param core      Plugin
+     * @param key       key (upper_case_plz)
+     * @param itemStack item
+     * @return number
+     */
     public static int getEnchantLevel(JavaPlugin core, String key, ItemStack itemStack) {
         ItemMeta meta = itemStack.getItemMeta();
         if (meta == null) {
