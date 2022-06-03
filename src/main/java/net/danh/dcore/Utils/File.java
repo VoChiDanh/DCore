04a5195339a58ed40345fc47503a0b1286086685
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-06-03 13:06:21 +0700

    add File.updateFile();

---
+++ src/main/java/net/danh/dcore/Utils/File.java
@@ -0,0 +1,56 @@
+package net.danh.dcore.Utils;
+
+import net.danh.dcore.DCore;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class File {
+
+    /**
+     * @param core Plugin
+     * @param get FileConfiguration
+     * @param file File.yml
+     */
+    public static void updateFile(JavaPlugin core, FileConfiguration get, String file) {
+        HashMap<String, Object> newConfig = getFileVals(core, file);
+        for (String var : get.getKeys(false)) {
+            newConfig.remove(var);
+        }
+        if (newConfig.size() != 0) {
+            for (String key : newConfig.keySet()) {
+                get.set(key, newConfig.get(key));
+            }
+            DCore.dCoreLog("Updating file " + file);
+            try {
+                get.save(new java.io.File(core.getDataFolder(), file));
+                DCore.dCoreLog("Update file " + file + " completed");
+            } catch (IOException ignored) {
+            }
+        }
+    }
+
+    private static HashMap<String, Object> getFileVals(JavaPlugin core, String file) {
+        HashMap<String, Object> var = new HashMap<>();
+        YamlConfiguration config = new YamlConfiguration();
+        try {
+            config.loadFromString(stringFromInputStream(core.getClass().getResourceAsStream("/" + file)));
+        } catch (InvalidConfigurationException ignored) {
+        }
+        for (String key : config.getKeys(false)) {
+            var.put(key, config.get(key));
+        }
+        return var;
+    }
+
+    private static String stringFromInputStream(InputStream in) {
+        return new Scanner(in).useDelimiter("\\A").next();
+    }
+}
+
