3f9d1763215d64ce19eedb99ac249dd85342fb7a
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-09-07 00:19:18 +0700

    Fix colorize

--- src/main/java/net/danh/dcore/Utils/Chat.java
+++ src/main/java/net/danh/dcore/Utils/Chat.java
@@ -10,40 +10,23 @@ import java.util.regex.Pattern;
  */
 public class Chat {
 
-    private final static char COLOR_CHAR = ChatColor.COLOR_CHAR;
-
-    /**
-     * @param input RString
-     * @return string with color code
-     */
-    public static String colorize(String input) {
-
-        input = ChatColor.translateAlternateColorCodes('&', input);
-        if (input.startsWith("&#")) {
-            input = translateHexColorCodes(input);
-        }
-
-        return input;
-    }
-
-
-    /**
-     * @param message message
-     * @return message with hex color
-     */
-    private static String translateHexColorCodes(String message) {
-
-        final Pattern hexPattern = Pattern.compile("&#" + "([A-Fa-f0-9]{6})" + "");
-        Matcher matcher = hexPattern.matcher(message);
-        StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
-
+    public static String colorize(String message) {
+        Pattern pattern = Pattern.compile("#[a-fA-F0-9]{6}");
+        Matcher matcher = pattern.matcher(message);
         while (matcher.find()) {
+            String hexCode = message.substring(matcher.start(), matcher.end());
+            String replaceSharp = hexCode.replace('#', 'x');
 
-            String group = matcher.group(1);
-            matcher.appendReplacement(buffer, COLOR_CHAR + "x" + COLOR_CHAR + group.charAt(0) + COLOR_CHAR + group.charAt(1) + COLOR_CHAR + group.charAt(2) + COLOR_CHAR + group.charAt(3) + COLOR_CHAR + group.charAt(4) + COLOR_CHAR + group.charAt(5));
+            char[] ch = replaceSharp.toCharArray();
+            StringBuilder builder = new StringBuilder();
+            for (char c : ch) {
+                builder.append("&").append(c);
+            }
 
+            message = message.replace(hexCode, builder.toString());
+            matcher = pattern.matcher(message);
         }
-
-        return matcher.appendTail(buffer).toString();
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 }
+
