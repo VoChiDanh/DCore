d252b3ef5f8bd47d4ba370f43126077da198ddf2
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-09-24 04:35:26 +0700

    Improve getInt and getDouble

--- src/main/java/net/danh/dcore/Random/Number.java
+++ src/main/java/net/danh/dcore/Random/Number.java
@@ -22,12 +22,30 @@ public class Number {
     }
 
     /**
+     * @param min min
+     * @param max max
+     * @return random
+     */
+    public static double getRandomDouble(double min, double max) {
+        if (max >= min + 2) {
+            Random r = new Random();
+            return r.nextDouble(max - min) + min;
+        } else {
+            return min;
+        }
+    }
+
+    /**
      * @param s RString
      * @return true/false
      */
     public static int getInt(java.lang.String s) {
         try {
-            return Integer.parseInt(s);
+            if (!s.contains("-")) {
+                return Integer.parseInt(s);
+            } else {
+                return getRandomInt(Integer.parseInt(s.split("-")[0]), Integer.parseInt(s.split("-")[1]));
+            }
         } catch (NumberFormatException | NullPointerException e) {
             return 0;
         }
@@ -39,7 +57,11 @@ public class Number {
      */
     public static double getDouble(java.lang.String s) {
         try {
-            return Double.parseDouble(s);
+            if (!s.contains("-")) {
+                return Double.parseDouble(s);
+            } else {
+                return getRandomDouble(Double.parseDouble(s.split("-")[0]), Double.parseDouble(s.split("-")[1]));
+            }
         } catch (NumberFormatException | NullPointerException e) {
             return 0d;
         }
